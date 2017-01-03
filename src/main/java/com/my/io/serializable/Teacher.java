fe832e8bce48440ddafe7b48da9fbded82e9c0d5
anlei <1518680150@qq.com>
2017-01-04 07:29:06 +0800

    Add Serializable

---
+++ src/main/java/com/my/io/serializable/Teacher.java
@@ -0,0 +1,33 @@
+package com.my.io.serializable;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2017/1/4.
+ */
+public class Teacher implements Serializable{
+
+    private String name;
+    private Person student;
+
+    public Teacher(String name, Person student){
+        this.name = name;
+        this.student = student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getStudent() {
+        return student;
+    }
+
+    public void setStudent(Person student) {
+        this.student = student;
+    }
+}
---
+++ src/main/java/com/my/io/serializable/WriteObject.java
@@ -0,0 +1,21 @@
+package com.my.io.serializable;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by Administrator on 2017/1/4.
+ */
+public class WriteObject {
+
+    public static void main(String[] args) throws Exception {
+        String filePath ="D:\\git\\accumulation\\src\\main\\resources\\serializable\\object.txt";
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            Person p = new Person("anlei", 20);
+            oos.writeObject(p);
+        }catch (IOException io){
+            io.printStackTrace();
+        }
+    }
+}
