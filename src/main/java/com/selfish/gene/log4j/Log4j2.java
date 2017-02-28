48a37562d2846deee426718c7962affa2e13d41f
anlei <1518680150@qq.com>
2017-02-28 20:08:25 +0800

    Add knowledge of log4j2

---
+++ src/main/java/com/selfish/gene/log4j/Log4j2.java
@@ -0,0 +1,55 @@
+package com.selfish.gene.log4j;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.io.*;
+
+/**
+ * Created by Administrator on 2017/2/27.
+ */
+public class Log4j2 {
+
+    private static final Logger LOGGER = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+    private static final Logger CONSOLE_LOGGER = LogManager.getLogger("console_log");
+//    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static void main(String[] args) throws Exception {
+
+        CONSOLE_LOGGER.trace("This is trace level");
+        CONSOLE_LOGGER.debug("This is debug level");
+        CONSOLE_LOGGER.info("This is info level");
+        CONSOLE_LOGGER.warn("This is warn level");
+        CONSOLE_LOGGER.error("This is error level");
+        CONSOLE_LOGGER.fatal("This is fatal level");
+
+    }
+
+    public Logger getLogger(String logger4j2Path, String loggerName){
+        File file = new File(logger4j2Path);
+        if(file == null){
+            LOGGER.error("The file with name of log4j2.xml is not found");
+            return null;
+        }
+        if(StringUtils.isEmpty(loggerName)){
+           loggerName = "";
+        }
+        Logger logger = null;
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+            final ConfigurationSource source = new ConfigurationSource(bis);
+            Configurator.initialize(null, source);
+            logger = LogManager.getLogger(loggerName);
+        } catch (FileNotFoundException e) {
+            LOGGER.error("The file with name of log4j2.xml is not found" + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            LOGGER.error("IO error" + e.getMessage());
+            e.printStackTrace();
+        }
+        return logger;
+    }
+}
